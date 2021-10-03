e8ec7d9cde7b551632dcf5aa653b258ee6fb79d9
infocity-miura <miura@infocity.co.jp>
2021-10-03 17:20:35 +0900

    Hello Worldを表示

---
+++ backend/db/session.py
@@ -0,0 +1,27 @@
+from typing import Generator
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings
+from typing import Generator
+
+
+# # For PostgreSQL
+# SQLALCHEMY_DATABASE_URL = settings.POSTGRES_URL
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# For SQLite
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
